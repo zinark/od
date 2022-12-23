fb9903cc2b4b82da2cc5f2ea2a0bb9be3a688501
ferhat <ceferhat@gmail.com>
2022-12-23 23:23:21 +0300

    initial

---
+++ looksrare.org/_next/static/0Qf_LYJ68KZ8TdoWJWGOe/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002F500","\u002Faccounts\u002Fimport","\u002Faccounts\u002Freview-orders","\u002Fbuy-looksrare-token","\u002Fcollections","\u002Fcollections\u002F[address]\u002Factivity","\u002Fexplore","\u002Fexplore\u002Factivity","\u002Frewards","\u002Frewards\u002Ftrading"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
